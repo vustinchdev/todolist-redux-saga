3a2f780c23ef56dd118cc7fb12888fe70bd33667
vustinchdev <vustinchdev@gmail.com>
2024-05-08 13:12:18 +0300

    redux-saga middleware

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,6 +4,7 @@ import {applyMiddleware, combineReducers, createStore} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import {appReducer} from './app-reducer'
 import {authReducer} from '../features/Login/auth-reducer'
+import createSagaMiddleware from 'redux-saga'
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
@@ -13,8 +14,10 @@ const rootReducer = combineReducers({
     app: appReducer,
     auth: authReducer
 })
+
+const sagaMiddleware = createSagaMiddleware()
 // непосредственно создаём store
-export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, sagaMiddleware));
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
